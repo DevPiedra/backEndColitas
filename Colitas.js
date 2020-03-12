352793355d3f541a6f41622614867af7775623d0
DrRockStone <depiedra@gmail.com>
2020-03-11 21:29:02 -0600

    Inicio Proyecto

---
+++ Colitas.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const URL_MONGO = "mongodb+srv://dapiedra:danielpa@cluster0-c8vti.gcp.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(URL_MONGO,{useNewUrlParser:true},function(err) {
+    if (!err){
+        console.log('Conexion exitosa')
+    }
+})
+
